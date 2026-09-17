1c021a7d7854cb71073ff084a7a6092e231f81ef
whostmdz <141586328+whostmdz@users.noreply.github.com>
2026-09-17 11:29:21 +0200

    init

---
+++ src/Vues/v_pied.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Vue Pied de page
+ *
+ * PHP Version 8
+ *
+ * @category  PPE
+ * @package   GSB
+ * @author    Réseau CERTA <contact@reseaucerta.org>
+ * @author    José GIL <jgil@ac-nice.fr>
+ * @copyright 2026 Réseau CERTA
+ * @license   Réseau CERTA
+ * @version   GIT: <0>
+ * @link      http://www.reseaucerta.org Contexte « Laboratoire GSB »
+ * @link      https://getbootstrap.com/docs/3.3/ Documentation Bootstrap v3
+ */
+
+?>
+        </div>
+		<footer class="footer">
+			<div class="container">
+				<p class="text-muted">© GSB 2027</p>
+			</div>
+		</footer>
+    </body>
+</html>
+
+
---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gsb/applifrais',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gsb/applifrais' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
