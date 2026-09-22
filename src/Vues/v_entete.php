6a8725b17bce03c160772618e715fa69620175da
whostmdz <141586328+whostmdz@users.noreply.github.com>
2026-09-22 15:39:35 +0200

    CSS bootstrap + entête modification accueil

--- src/Vues/v_entete.php
+++ src/Vues/v_entete.php
@@ -26,7 +26,10 @@
         <meta name="description" content="">
         <meta name="author" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="./styles/bootstrap/bootstrap.css" rel="stylesheet">
+        ////<link href="./styles/bootstrap/bootstrap.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://jsdelivr.net">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
         <link href="./styles/style.css" rel="stylesheet">
     </head>
     <body>
@@ -46,31 +49,32 @@
                     </div>
                     <div class="col-md-8">
                         <ul class="nav nav-pills pull-right" role="tablist">
-                            <li <?php if (!$uc || $uc == 'accueil') { ?>class="active" <?php } ?>>
-                                <a href="index.php">
-                                    <span class="glyphicon glyphicon-home"></span>
+                        <li class="nav-item">
+                            <a class="nav-link <?php if (!$uc || $uc == 'accueil') echo 'active'; ?>"href="index.php">
+                                <i class="bi bi-house-door"></i>
                                     Accueil
-                                </a>
-                            </li>
-                            <li <?php if ($uc == 'gererFrais') { ?>class="active"<?php } ?>>
-                                <a href="index.php?uc=gererFrais&action=saisirFrais">
-                                    <span class="glyphicon glyphicon-pencil"></span>
+                            </a>
+                        </li>
+                        <li <?php if ($uc == 'gererFrais') { ?>class="active"<?php } ?>>
+                            <a href="index.php?uc=gererFrais&action=saisirFrais">
+                                <i class="bi bi-pencil"></i>
                                     Renseigner la fiche de frais
-                                </a>
-                            </li>
-                            <li <?php if ($uc == 'etatFrais') { ?>class="active"<?php } ?>>
-                                <a href="index.php?uc=etatFrais&action=selectionnerMois">
-                                    <span class="glyphicon glyphicon-list-alt"></span>
+                            </a>
+                        </li>
+
+                        <li <?php if ($uc == 'etatFrais') { ?>class="active"<?php } ?>>
+                            <a href="index.php?uc=etatFrais&action=selectionnerMois">
+                                <i class="bi bi-card-list"></i>
                                     Afficher mes fiches de frais
-                                </a>
-                            </li>
-                            <li 
-                            <?php if ($uc == 'deconnexion') { ?>class="active"<?php } ?>>
-                                <a href="index.php?uc=deconnexion&action=demandeDeconnexion">
-                                    <span class="glyphicon glyphicon-log-out"></span>
+                            </a>
+                        </li>
+
+                        <li <?php if ($uc == 'deconnexion') { ?>class="active"<?php } ?>>
+                            <a href="index.php?uc=deconnexion&action=demandeDeconnexion">
+                                <i class="bi bi-box-arrow-right"></i>
                                     Déconnexion
-                                </a>
-                            </li>
+                            </a>
+                        </li>
                         </ul>
                     </div>
                 </div>
