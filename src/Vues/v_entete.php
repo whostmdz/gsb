721b3289c718f543eaf68e1121f4e5ff24453702
whostmdz <thomasdiemoz@gmail.com>
2026-09-22 21:31:57 +0200

    update

--- src/Vues/v_entete.php
+++ src/Vues/v_entete.php
@@ -25,10 +25,7 @@
         <meta name="description" content="">
         <meta name="author" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        ////<link href="./styles/bootstrap/bootstrap.css" rel="stylesheet">
-        <link rel="stylesheet" href="https://jsdelivr.net">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        <link href="./styles/bootstrap/bootstrap.css" rel="stylesheet">
         <link href="./styles/style.css" rel="stylesheet">
     </head>
     <body>
@@ -46,33 +43,6 @@
                                  title="Laboratoire Galaxy-Swiss Bourdin">
                         </h1>
                     </div>
-<<<<<<< HEAD
-                    <div class="col-md-8">
-                        <ul class="nav nav-pills pull-right" role="tablist">
-                        <li class="nav-item">
-                            <a class="nav-link <?php if (!$uc || $uc == 'accueil') echo 'active'; ?>"href="index.php">
-                                <i class="bi bi-house-door"></i>
-                                    Accueil
-                            </a>
-                        </li>
-                        <li <?php if ($uc == 'gererFrais') { ?>class="active"<?php } ?>>
-                            <a href="index.php?uc=gererFrais&action=saisirFrais">
-                                <i class="bi bi-pencil"></i>
-                                    Renseigner la fiche de frais
-                            </a>
-                        </li>
-
-                        <li <?php if ($uc == 'etatFrais') { ?>class="active"<?php } ?>>
-                            <a href="index.php?uc=etatFrais&action=selectionnerMois">
-                                <i class="bi bi-card-list"></i>
-                                    Afficher mes fiches de frais
-                            </a>
-                        </li>
-
-                        <li <?php if ($uc == 'deconnexion') { ?>class="active"<?php } ?>>
-                            <a href="index.php?uc=deconnexion&action=demandeDeconnexion">
-                                <i class="bi bi-box-arrow-right"></i>
-=======
                     <div class="col-lg-8">
                         <ul class="nav nav-pills justify-content-lg-end gap-1" aria-label="Navigation principale">
                             <li class="nav-item">
@@ -104,10 +74,9 @@
                                    <?php if ($uc == 'deconnexion') { ?>aria-current="page"<?php } ?>
                                    href="index.php?uc=deconnexion&action=demandeDeconnexion">
                                     <svg class="icon" aria-hidden="true" focusable="false"><use href="./images/icons.svg#log-out"></use></svg>
->>>>>>> f7662d3 (Migration bootstrap + FICHIER PROGRESSION et CAHIER DES CHARGES)
                                     Déconnexion
-                            </a>
-                        </li>
+                                </a>
+                            </li>
                         </ul>
                     </div>
                 </div>
